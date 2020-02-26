e3322e097473be80046aa70d40293dd7a0e1023b
Kiran <kirankumardash1@gmail.com>
2020-02-26 22:02:21 +0530

    init commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 //const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api';
-
+//process.env.MONGODB_URL
 mongoose.connect(process.env.MONGODB_URL,{
     useNewUrlParser:true,
     useCreateIndex:true,
